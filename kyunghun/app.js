f8dc6288a531883d1c70f3a749cf7ec2cbcc15d2
강경훈 <kyunghun@gang-gyeonghuns-MacBook-Pro.local>
2023-02-22 12:21:51 +0900

    [ADD] express 초기셋팅

---
+++ kyunghun/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const logger = require('morgan')
+const app = express()
+require("dotenv").config();
+
+const PORT = process.env.PORT
+app.use(cors())
+app.use(logger('dev'));
+ 
+app.get('/ping', function (req, res, next) {
+  res.json({message: 'pong'})
+})
+ 
+app.listen(PORT, function () {
+  console.log(`server listening on port ${PORT}`)
+})
