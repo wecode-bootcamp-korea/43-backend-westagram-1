5d34c915f409b4418da5f4f096b17fb9aa271baa
jang_jiwon <kooriangman@gmail.com>
2023-02-23 10:42:09 +0900

    [장지원] Assignment 2: Express API - 유저 회원가입 하기

--- jiwonjang/app.js
+++ jiwonjang/app.js
@@ -1,13 +1,11 @@
-const dotenv = require("dotenv");
-dotenv.config();
-const http = require("http");
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 
 const { DataSource } = require("typeorm");
 
-const myDataSource = new DataSource({
+const appDataSource = new DataSource({
   type: process.env.TYPEORM_CONNECTION,
   host: process.env.TYPEORM_HOST,
   port: process.env.TYPEORM_PORT,
@@ -16,9 +14,14 @@ const myDataSource = new DataSource({
   database: process.env.TYPEORM_DATABASE,
 });
 
-myDataSource.initialize().then(() => {
-  console.log("Data Source has been initialized!");
-});
+appDataSource
+  .initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization", err);
+  });
 
 const app = express();
 
@@ -26,12 +29,11 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
 
-const server = http.createServer(app);
 const PORT = process.env.PORT;
 
 //health check
 app.get("/ping", (req, res) => {
-  res.status(200).json({ message: "pong" });
+  return res.status(200).json({ message: "pong" });
 });
 
 //create users
@@ -41,7 +43,7 @@ app.post("/users", async (req, res) => {
 
   //console.log(req)
 
-  await myDataSource.query(
+  await appDataSource.query(
     `INSERT INTO users(
       name,
       email,
