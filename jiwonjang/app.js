e03d33a344debc1ce9822916de8aa9c08cf2409a
jang_jiwon <kooriangman@gmail.com>
2023-02-23 10:23:09 +0900

    [장지원] Assignment 2: Express API -  유저 회원가입 하기

--- jiwonjang/app.js
+++ jiwonjang/app.js
@@ -26,15 +26,41 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
 
-app.get("/ping", (req, res) => {
-  res.json({ message: "pong" });
-});
-
 const server = http.createServer(app);
 const PORT = process.env.PORT;
 
+//health check
+app.get("/ping", (req, res) => {
+  res.status(200).json({ message: "pong" });
+});
+
+//create users
+
+app.post("/users", async (req, res) => {
+  const { name, email, profileImage, password } = req.body;
+
+  //console.log(req)
+
+  await myDataSource.query(
+    `INSERT INTO users(
+      name,
+      email,
+      profile_image,
+      password
+    ) VALUES (?, ?, ?, ?);
+    `,
+    [name, email, profileImage, password]
+  );
+  res.status(201).json({ message: "useCreated" });
+});
+
+const HOST = process.env.HOST;
 const start = async () => {
-  server.listen(PORT, () => console.log(`server is listening on ${PORT}`));
+  try {
+    app.listen(PORT, HOST, () => console.log(`server is listening on ${PORT}`));
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 start();
