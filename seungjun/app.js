89aa085352361f47c7dd164d8c5d5bdffb5a6c75
류승준 <ryuseungjun@lyuseungjun-ui-MacBookPro.local>
2023-02-23 19:08:26 +0900

    [류승준] Assignment 1:Express API - 초기 환경 세팅

--- seungjun/app.js
+++ seungjun/app.js
@@ -1,14 +1,12 @@
-const http = require ("http");
+require("dotenv").config();
+
 const express = require ("express");
 const cors = require ("cors");
 const morgan = require ("morgan");
 
-const dotenv = require ("dotenv");
-dotenv.config()
-
 const { DataSource } = require('typeorm')
 
-const myDataSource = new DataSource({
+const appDataSource = new DataSource({
     type: process.env.TYPEORM_CONNECTION,
     host: process.env.TYPEORM_HOST,
     port: process.env.TYPEORM_PORT,
@@ -17,10 +15,13 @@ const myDataSource = new DataSource({
     database: process.env.TYPEORM_DATABASE
 })
 
-myDataSource.initialize()
+appDataSource.initialize()
  .then(() => {
     console.log("Data Source has been initialized!")
- });
+ })
+ .catch((err) => {
+    console.error("Error duing Data Source initialization:", err)
+ })
 
 const app = express();
 
@@ -33,10 +34,7 @@ app.get("/ping", (req, res) => {
     res.status(200).json({ message : "pong"});
 })
 
-const server = http.createServer(app)
 const PORT = process.env.PORT;
-
-
 
 const start = async () => {
     app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
